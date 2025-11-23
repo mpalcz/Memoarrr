f9e953ecda8fffedbce8ef107fbacf5178b951ac
Akash Prasad <akashprasad19@gmail.com>
2025-11-22 20:06:47 -0500

    Finished Rubis class and header code

--- Rubis.cpp
+++ Rubis.cpp
@@ -1,10 +1,20 @@
 #include "Rubis.h"
+#include <stdexcept>
 using namespace std;
 
-int Rubis::possibleNumberOfRubis[4] = {1, 2, 3, 4};
+// constructor
+Rubis::Rubis(int val) : numRubis(val)
+{
+    // should never happen since we will handle before to allow retries but just in case
+    if (val > 4 || val < 1)
+    {
+        throw invalid_argument("Rubi value must be between 1 and 4");
+    }
+}
 
 // Operator<< definition
-ostream& operator<<(ostream& os, const Rubis& obj) {
-    os << obj.numRubis;
+ostream &operator<<(ostream &os, const Rubis &obj)
+{
+    os << obj.numRubis << " Rubis";
     return os;
 }
--- Rubis.h
+++ Rubis.h
@@ -3,23 +3,21 @@
 
 #include <iostream>
 
-
 class RubisDeck;
 
-class Rubis {
-    private:
-        // friend classes
-        friend class RubisDeck;
+class Rubis
+{
+    friend class RubisDeck; // establish friendship for private constructor access
+    friend std::ostream &operator<<(std::ostream &os, const Rubis &obj);
 
-        // Constructor
-        Rubis(int);
-        int numRubis;
-    
-    public:
-        static int possibleNumberOfRubis[4];
+private:
+    int numRubis;
+    Rubis(int val); // private constructor
+    ~Rubis();
 
-        // Friend functions
-        friend std::ostream& operator<<(std::ostream& os, const Rubis& obj);
+public:
+    // int conversion operator (const since we do not change the object)
+    operator int() const { return numRubis; }
 };
 
 #endif
