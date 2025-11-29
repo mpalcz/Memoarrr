cd606e4e852e423b80668f5696b98c4679b5e063
Akash Prasad <akashprasad19@gmail.com>
2025-11-29 16:20:40 -0500

    Added Rules

--- source/Game.h
+++ source/Game.h
@@ -12,17 +12,25 @@ class Game {
     std::vector<Player> players;
     const Card *current = nullptr; // const since these are just pointers not changing the Card
     const Card *previous = nullptr;
+    Player::Side turn = Player::Side::top; // keep track of whos turn it is (top by default)
 
   public:
     Game(); // default constructor, we will add players after construction
-    int getRound() { return round; }
+    int getRound() const { return round; }
     void addPlayer(const Player &);
     Player &getPlayer(Player::Side);
-    const Card *getPreviousCard() { return previous; }
-    const Card *getCurrentCard() { return current; }
+    const Card *getPreviousCard() const { return previous; } // return a const and also make the method const since we arent changing anyhting
+    const Card *getCurrentCard() const { return current; }   // can only call const methods on a const object (relevant for Rules class)
     void setCurrentCard(const Card *c);
     Card *getCard(const Board::Letter &, const Board::Number &);
     void setCard(const Board::Letter &, const Board::Number &, Card *);
     friend std::ostream &operator<<(std::ostream &os, const Game &g); // cout << operator
+
+    const std::vector<Player> &getPlayers() const { return players; } // let others check the players but not modify them
+    Player::Side getTurn() const { return turn; }                     // get the player whos turn it is
+    void rotateTurn() {
+        // cast the integer back to an enum and have turn circle back based on number of players
+        turn = static_cast<Player::Side>((static_cast<int>(turn) + 1) % players.size());
+    }
 };
 #endif
---
+++ source/Rules.cpp
@@ -0,0 +1,31 @@
+#include <Card.h>
+#include <Game.h>
+#include <Rules.h>
+
+bool Rules::isValid(const Game &g) {
+    const Card *current = g.getCurrentCard();
+    const Card *previous = g.getPreviousCard();
+
+    // equal if either the background colour or the animal are the same
+    bool animal_equal = FaceAnimal(*current) == FaceAnimal(*previous);
+    bool bg_equal = FaceBackground(*current) == FaceBackground(*previous);
+
+    return animal_equal || bg_equal;
+}
+bool Rules::gameOver(const Game &g) { return g.getRound() == 6; }
+bool Rules::roundOver(const Game &g) {
+    // check if all players in the game are active
+    const std::vector<Player> &players = g.getPlayers(); // const method call
+    for (int i = 0; i < players.size(); ++i) {
+        if (players[i].isActive()) {
+            return false; // at least one player is still active so the round is not over
+        }
+    }
+    return true;
+}
+const Player &Rules::getNextPlayer(const Game &g) {
+    const std::vector<Player> &players = g.getPlayers();
+    int currentIndex = static_cast<int>(g.getTurn());
+    int nextIndex = (currentIndex + 1) % players.size();
+    return players[nextIndex];
+}
